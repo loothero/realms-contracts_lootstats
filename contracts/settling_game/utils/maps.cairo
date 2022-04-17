1e77f97c2ef8923998c2228817d78af7f1999dee
threepwave <threepwave@gmail.com>
2022-04-17 05:42:03 +0000

    first pass at set_bit with a single error around dynamic arrays

--- contracts/settling_game/utils/maps.cairo
+++ contracts/settling_game/utils/maps.cairo
@@ -3,18 +3,37 @@
 #
 # MIT License
 
+# TODO - Delete @external once we deploy.
+
 %lang starknet
 
-from starkware.cairo.common.cairo_builtins import HashBuiltin, SignatureBuiltin
-from starkware.cairo.common.math import (unsigned_div_rem)
+from starkware.cairo.common.cairo_builtins import HashBuiltin, SignatureBuiltin, BitwiseBuiltin
+from starkware.cairo.common.bitwise import bitwise_and
+from starkware.cairo.common.pow import pow
+from starkware.cairo.common.math import (
+    assert_nn_le,
+    unsigned_div_rem)
+
+# Writes a bit (1) on a position within the map
+@external
+func set_bit{
+    syscall_ptr : felt*, bitwise_ptr : BitwiseBuiltin*, range_check_ptr
+}(size : felt, map_len : felt, map : felt*, position : felt) -> (map_len : felt, map : felt*):
+    alloc_locals
+    let (local quotient, remainder) = unsigned_div_rem(size*size, 251)
+    assert_nn_le(position, 251 + (quotient * 251))    # Make sure the position fits inside our array(s)
+    let (mask) = pow(2, position)
+    let (masked) = bitwise_and(map[quotient], mask)
+    map[quotient] = masked
+
+    return(map_len, map)
+end
 
 # Calculates the number of felts needed based on the size of a map (e.g. 8x8 -> 1)
-# HACK - Delete @external once we deploy.
 @external   
 func calc_size{
     syscall_ptr : felt*, range_check_ptr
 }(size : felt) -> (length : felt):
-    let totalSize = size * size
-    let (divisor, remainder) = unsigned_div_rem(totalSize, 251)
-    return(divisor+1)
+    let (quotient, _) = unsigned_div_rem(size*size, 251) # Figure out how many felts (251 bits) we need as one bit is one map tile
+    return(quotient+1)
 end
--- tests/settling_game/utils/maps_test.py
+++ tests/settling_game/utils/maps_test.py
@@ -1,4 +1,5 @@
 # maps_test.py - Test for maps data structure used for dungeon layout, enemies, etc. 
+# TODO: Convert to the proper test format in the rest of the repo
 #
 # MIT License
 import os
@@ -7,10 +8,41 @@ import pytest
 
 
 from starkware.starknet.testing.starknet import Starknet
+from starkware.starkware_utils.error_handling import StarkException
 
 # path to the contract source code.
 CONTRACT_FILE = os.path.join(
     os.path.dirname(__file__), "../../../", "contracts/settling_game/utils/maps.cairo")
+
+@pytest.mark.asyncio
+async def test_set_bit():
+    starknet = await Starknet.empty()
+
+    # Deploy the utils contract.
+    contract = await starknet.deploy(
+        source=CONTRACT_FILE,
+    )
+   
+    # Single Felt Length: Set a valid bit (within 1-250)
+    layout = await contract.set_bit(size=5, map=[1], position=215).call()
+    assert layout.result == ([1],)
+
+    # Multi-Felt Length: Set a valid bit (within 252-502 for a 2-length array)
+    # Multi-Felt Length: Set an invalid bit (outside 252-502 for a 2-length array)
+
+@pytest.mark.asyncio
+async def test_set_bit_exceptions():
+    with pytest.raises(StarkException):
+        starknet = await Starknet.empty()
+
+        # Deploy the utils contract.
+        contract = await starknet.deploy(
+            source=CONTRACT_FILE,
+        )
+    
+        # Single Felt Length: Set an invalid bit (outside of 1-250)
+        layout = await contract.set_bit(size=5, map=[1], position=255).call()
+        assert layout.result == ([1],)
 
 @pytest.mark.asyncio
 async def test_calc_size():
@@ -21,8 +53,7 @@ async def test_calc_size():
         source=CONTRACT_FILE,
     )
 
-    ## Get size of a contract that is < 1 felt (1)
-    # Make sure calc_size returns correctly
+    # Get size of a contract that is < 1 felt (1)
     size = await contract.calc_size(size=5).call()
     assert size.result == (1,)
 
